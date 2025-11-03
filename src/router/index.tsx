e6486d0b4e844fdf370a71116f5c55517b928148
taiseiiiii <isemiya.0509@gmail.com>
2025-11-02 23:55:41 -0800

    fix: path alias

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
-import { RootLayout } from "../shared/components/layout/RootLayout";
-import { HomePage } from "../features/pages/HomePage";
-import { CountryDetailPage } from "../features/pages/CountryDetailPage";
+import { RootLayout } from "@/shared/components/layout/RootLayout";
+import { CountryDetailPage } from "@/features/pages/CountryDetailPage";
+import { HomePage } from "@/features/pages/HomePage";
 
 export const router = createBrowserRouter([
   {
